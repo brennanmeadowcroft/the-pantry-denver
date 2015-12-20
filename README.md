e157d7e40faa4337e0ec0deb0f0a5868d0cc96cf
Brennan Meadowcroft <bmeadowcroft@itriagehealth.com>
2015-12-20 15:40:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# CFHero - Public Site
+
+## Running the application
+The site is based in the public folder.  The development version includes an ExpressJS server:
+```
+npm start
+```
+
+
+
+## Resources
+[Landing Page](http://startbootstrap.com/template-overviews/landing-page/)
---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+
+var app = express()
+app.use(express.static('public'))
+
+app.use('/', express.static('public'))
+// app.get('/', function(req, res) {
+//   res.sendfile('public/');
+// });
+
+var port = process.env.PORT || 5000
+app.listen(port, function () {
+  console.log('Listening on port ' + port)
+})
